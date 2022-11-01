c72104977dedaa19e28636af87b3f7f390949fe3
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2022-11-01 19:22:12 +0700

    Handle function as Rollup config

--- bin/build-z.js
+++ bin/build-z.js
@@ -1,13 +1,14 @@
 #!/usr/bin/env node
 import { ProjectConfig } from '@run-z/project-config';
 import { ProjectRollupConfig } from '@run-z/project-config/rollup';
+import process from 'node:process';
 
 const project = await ProjectConfig.load();
 const config = ProjectRollupConfig.of(project);
 const options = await project.loadConfig('./rollup.config.js', null);
 
 if (options) {
-  config.replaceOptions(options);
+  config.replaceOptions(typeof options === 'function' ? options(process.argv.slice(2)) : options);
 }
 
 await config.run();
